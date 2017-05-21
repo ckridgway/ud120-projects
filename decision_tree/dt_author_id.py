4e30afd82a8e499fac1550e07a9c11ed6bd45feb
Chris Ridgway <ckridgway@gmail.com>
2017-05-20 20:35:36 -0500

    Complete Decision Tree chapter.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -19,13 +19,34 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
-### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+print(len(features_train[0]))
 
+# Create and train the Decision Tree classifier
+classifier = DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+classifier.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+predictions = classifier.predict(features_test)
+print("prediction time:", round(time()-t1, 3), "s")
+
+print("Accuracy Score=", accuracy_score(labels_test, predictions))
+
+WHO = ['Sara', 'Chris']
+
+print('Item 10=', WHO[predictions[10]])
+print('Item 26=', WHO[predictions[26]])
+print('Item 50=', WHO[predictions[50]])
+
+import numpy
+unqiue, counts = numpy.unique(predictions, return_counts=True)
+
+print('Sara wrote {} emails.'.format(counts[0]))
+print('Chris wrote {} email.'.format(counts[1]))
 #########################################################
-
-
